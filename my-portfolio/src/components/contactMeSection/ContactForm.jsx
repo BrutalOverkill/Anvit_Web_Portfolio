7a05d4bf856a9ab6078e77a320615eecda4296eb
BrutalOverkill <anvit19960@gmail.com>
2025-03-04 20:05:24 +0530

    Done with the Contact me section

--- my-portfolio/src/components/contactMeSection/ContactForm.jsx
+++ my-portfolio/src/components/contactMeSection/ContactForm.jsx
@@ -1,36 +1,82 @@
-import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-    const form = useRef();
-
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs
-        .sendForm('service_9twg1bk', 'template_n5gf1af', form.current, {
-          publicKey: 'oBxMbYFqGQSNhMLbH',
-        })
-        .then(
-          () => {
-            console.log('SUCCESS!');
-          },
-          (error) => {
-            console.log('FAILED...', error.text);
-          },
-        );
-    };
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState("");
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("service_9twg1bk", "template_n5gf1af", form.current, {
+        publicKey: "oBxMbYFqGQSNhMLbH",
+      })
+      .then(
+        () => {
+          setEmail("");
+          setName("");
+          setMessage("");
+          setSuccess("Message Sent Succesfully");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
 
   return (
     <div>
-      <form className="flex flex-col gap-4" ref={form} onSubmit={sendEmail}>
-        <input name="from_name" type="text" placeholder="Your Name" required className="h-12 rounded-lg bg-lightBrown px-2"/>
-        <input name="from_email" type="email" placeholder="Your Email" required className="h-12 rounded-lg bg-lightBrown px-2"/>
-        <textarea name="message" type="text" placeholder="Message" rows="9" cols="50" required className="rounded-lg bg-lightBrown p-2"/>
-        <button type="submit" className="w-full rounded-lg border-cyan text-white h-12 font-bold text-xl hover:bg-darkCyan bg-cyan transition-all duration-500">Send</button>
+      <p className="text-cyan">{success}</p>
+      <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-4">
+        <input
+          type="text"
+          name="from_name"
+          placeholder="Your Name"
+          required
+          className="h-12 rounded-lg bg-lightBrown px-2"
+          value={name}
+          onChange={handleName}
+        />
+        <input
+          type="email"
+          name="from_email"
+          placeholder="Your Email"
+          required
+          className="h-12 rounded-lg bg-lightBrown px-2"
+          value={email}
+          onChange={handleEmail}
+        />
+        <textarea
+          type="text"
+          name="message"
+          rows="9"
+          cols="50"
+          placeholder="Message"
+          required
+          className=" rounded-lg bg-lightBrown p-2"
+          value={message}
+          onChange={handleMessage}
+        />
+        <button
+          type="submit"
+          className="w-full rounded-lg border border-cyan text-white h-12 font-bold text-xl hover:bg-darkCyan bg-cyan transition-all duration-500"
+        >
+          Send
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
